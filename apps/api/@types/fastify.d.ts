8c134bd621718df372642c433092da8fb39f43bd
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-10-10 09:08:32 -0300

    feat: add membership middleware

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,15 @@
 import 'fastify'
 
+import { Member, Organization } from '@prisma/client'
+
+interface GetCurrentUserMembership {
+	organization: Organization
+	membership: Member
+}
+
 declare module 'fastify' {
 	export interface FastifyRequest {
 		getCurrentUserId(): Promise<string>
+		getCurrentUserMembership(slug: string): Promise<GetCurrentUserMembership>
 	}
 }
