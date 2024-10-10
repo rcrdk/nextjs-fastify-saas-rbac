4a60ad53f3635a4f94cfebe02c9ce3a608d19ec3
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-10-10 10:32:19 -0300

    fix: apply cascade delete to prisma schema

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -36,14 +36,21 @@ export async function resetPassword(app: FastifyInstance) {
 
 			const hashedPassword = await hash(password, 8)
 
-			await prisma.user.update({
-				where: {
-					id: tokenFromCode.userId,
-				},
-				data: {
-					passwordHash: hashedPassword,
-				},
-			})
+			await prisma.$transaction([
+				prisma.user.update({
+					where: {
+						id: tokenFromCode.userId,
+					},
+					data: {
+						passwordHash: hashedPassword,
+					},
+				}),
+				prisma.token.delete({
+					where: {
+						id: code,
+					},
+				}),
+			])
 
 			return reply.status(204).send()
 		},
