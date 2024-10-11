20856880a4052b0b37d1689b549e0a6285d22cc2
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-10-11 08:43:09 -0300

    feat: get organization invetes

---
+++ apps/api/src/http/routes/invites/get-invites.ts
@@ -0,0 +1,87 @@
+import { rolesSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getInvites(app: FastifyInstance) {
+	app
+		.withTypeProvider<ZodTypeProvider>()
+		.register(auth)
+		.get(
+			'/organization/:slug/invites',
+			{
+				schema: {
+					tags: ['Invites'],
+					summary: 'Get organization invites.',
+					security: [{ bearerAuth: [] }],
+					params: z.object({
+						slug: z.string(),
+					}),
+					response: {
+						200: z.object({
+							invites: z.array(
+								z.object({
+									id: z.string().uuid(),
+									email: z.string().email(),
+									role: rolesSchema,
+									createdAt: z.date(),
+									author: z
+										.object({
+											id: z.string().uuid(),
+											name: z.string().nullable(),
+										})
+										.nullable(),
+								}),
+							),
+						}),
+					},
+				},
+			},
+			async (request, reply) => {
+				const { slug } = request.params
+				const userId = await request.getCurrentUserId()
+
+				const { organization, membership } =
+					await request.getCurrentUserMembership(slug)
+
+				const { cannot } = getUserPermissions(userId, membership.role)
+
+				if (cannot('get', 'Invite')) {
+					throw new UnauthorizedError(
+						'You are not allowed to get organization invites.',
+					)
+				}
+
+				const invites = await prisma.invite.findMany({
+					where: {
+						organizationId: organization.id,
+					},
+					orderBy: {
+						createdAt: 'desc',
+					},
+					select: {
+						id: true,
+						email: true,
+						role: true,
+						createdAt: true,
+						author: {
+							select: {
+								id: true,
+								name: true,
+							},
+						},
+					},
+				})
+
+				return reply.status(201).send({
+					invites,
+				})
+			},
+		)
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -20,6 +20,7 @@ import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -92,6 +93,7 @@ app.register(updateMember)
 app.register(removeMember)
 
 app.register(createInvite)
+app.register(getInvites)
 app.register(getInvite)
 
 app.register(createProject)
