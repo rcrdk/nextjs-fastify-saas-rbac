57612f5656d98c074d035472cbf868d009bc8364
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-10-11 12:03:14 -0300

    feat: add create account and password recover pages, fixes on authenticantion page

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -1,4 +1,4 @@
-import { IconBrandGithub, IconMail } from '@tabler/icons-react'
+import { IconBrandGithub } from '@tabler/icons-react'
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
@@ -6,9 +6,14 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
 	return (
 		<form action="" className="flex flex-col space-y-4">
+			<div className="space-y-1">
+				<Label htmlFor="name">Name</Label>
+				<Input name="name" type="text" id="name" />
+			</div>
+
 			<div className="space-y-1">
 				<Label htmlFor="email">E-mail</Label>
 				<Input name="email" type="email" id="email" />
@@ -19,27 +24,24 @@ export default function SignInPage() {
 				<Input name="password" type="password" id="password" />
 			</div>
 
-			<Link
-				href="/auth/forgot-password"
-				className="self-start text-xs font-medium text-foreground hover:underline"
-			>
-				Forgot your password?
-			</Link>
+			<div className="space-y-1">
+				<Label htmlFor="confirm_password">Confirm your password</Label>
+				<Input name="confirm_password" type="password" id="confirm_password" />
+			</div>
 
 			<Button className="gap-2" type="submit">
-				<IconMail size={20} />
-				Sign-in with e-mail
+				Create account
 			</Button>
 
 			<Button className="gap-2" variant="link" size="sm" asChild>
-				<Link href="/auth/sign-up">Create new account</Link>
+				<Link href="/auth/sign-in">I already have an account</Link>
 			</Button>
 
 			<Separator />
 
 			<Button className="gap-2" variant="outline">
 				<IconBrandGithub size={20} />
-				Sign-in with GitHub
+				Sign-up with GitHub
 			</Button>
 		</form>
 	)
