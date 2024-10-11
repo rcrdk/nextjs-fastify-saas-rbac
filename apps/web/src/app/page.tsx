66a9fd6d9bc09019b3d86611f2b87a8af8780c3a
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-10-11 10:26:39 -0300

    feat: add shadcn-ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-	return <h1>Hello, world!</h1>
+	return (
+		<>
+			<h1>Hello, world!</h1>
+
+			<Button>Sign in</Button>
+		</>
+	)
 }
