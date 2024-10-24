d1f822a6463098c2508f8f751cb3774025ba911a
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-10-24 14:51:37 -0300

    feat: welcome logged user

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,18 +1,25 @@
-/* eslint-disable prettier/prettier */
+import { IconMoodHappy, IconUniverse } from '@tabler/icons-react'
 import { cookies } from 'next/headers'
 
+import { auth } from '@/auth'
 import { Footer } from '@/components/footer'
 import { Header } from '@/components/header'
 import { TriggerToastLoad } from '@/components/trigger-toast-load'
+import { getFirstName } from '@/utils/get-first-name'
 
 export default async function Home() {
 	const cookieStore = await cookies()
+	const { user } = await auth()
 
-	const transferedOrganizationMessage = cookieStore.get('@SAAS:transferedOrganization')
-	const deletedOrganizationMessage = cookieStore.get('@SAAS:deletedOrganization')
+	const transferedOrganizationMessage = cookieStore.get(
+		'@SAAS:transferedOrganization',
+	)
+	const deletedOrganizationMessage = cookieStore.get(
+		'@SAAS:deletedOrganization',
+	)
 
 	return (
-		<div className="flex min-h-screen flex-col px-5 py-3 md:px-8 sm:py-4">
+		<div className="flex min-h-screen flex-col px-5 py-3 sm:py-4 md:px-8">
 			<Header />
 
 			{deletedOrganizationMessage && (
@@ -30,10 +37,15 @@ export default async function Home() {
 			)}
 
 			<main className="mx-auto flex w-full max-w-[1200px] flex-grow items-center py-6 sm:py-8">
-				<div className="w-full space-y-4">
-					<h1 className="text-center text-2xl font-bold">
-						Select an organization
+				<div className="w-full space-y-4 text-center">
+					<IconUniverse strokeWidth={0.75} className="mx-auto size-20" />
+					<h1 className="text-4xl font-bold">
+						Hello, {getFirstName(user.name, 'human')}!
 					</h1>
+					<p className="text-balance text-muted-foreground">
+						It is nice to have you here. To get it started you can select
+						organization on the navigation menu above.
+					</p>
 				</div>
 			</main>
 
