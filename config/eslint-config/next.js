16c5e58a03e6db5e1fef6d4ff32cd81713381d55
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2025-02-28 19:56:51 -0300

    fix: eslint

--- config/eslint-config/next.js
+++ config/eslint-config/next.js
@@ -1,6 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-	extends: ['@rocketseat/eslint-config/next', 'next/core-web-vitals'],
+	extends: [
+		'@rocketseat/eslint-config/next',
+		'next/core-web-vitals',
+		'next/typescript',
+	],
 	plugins: ['simple-import-sort'],
 	rules: {
 		'simple-import-sort/imports': 'error',
